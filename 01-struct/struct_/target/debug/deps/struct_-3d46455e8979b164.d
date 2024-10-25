f6b8671424e8e65d4c026c5674602c39138b6d2a
Moi Baldenegro <124749789+MoiBaldenegro@users.noreply.github.com>
2024-10-25 00:01:31 -0600

    Delete 01-struct/struct_/target directory

--- 01-struct/struct_/target/debug/deps/struct_-3d46455e8979b164.d
+++
@@ -1,5 +0,0 @@
-/home/moibaldenegro/Documents/rust/rust-essentials/01-struct/struct_/target/debug/deps/struct_-3d46455e8979b164: src/main.rs
-
-/home/moibaldenegro/Documents/rust/rust-essentials/01-struct/struct_/target/debug/deps/struct_-3d46455e8979b164.d: src/main.rs
-
-src/main.rs:
--- 01-struct/struct_/target/debug/struct_.d
+++
@@ -1,1 +0,0 @@
-/home/moibaldenegro/Documents/rust/rust-essentials/01-struct/struct_/target/debug/struct_: /home/moibaldenegro/Documents/rust/rust-essentials/01-struct/struct_/src/main.rs
